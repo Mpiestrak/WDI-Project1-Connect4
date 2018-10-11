4a52f982ade1b44617d461a533ff1f4c56f62caa
Matt Piestrak <mattpiestrak@Matts-MacBook-Pro.local>
2018-10-10 21:35:35 -0400

    Added begin/reset button toggle upon click

--- main.js
+++ main.js
@@ -1,13 +1,27 @@
 // make object of each column with number as key, div as value
 
 // Adding class player1/player2 when clicked to change background color on lowest unfilled div to correct color
+var allSlots = $('.diamond')
 var player1Color = []
 var player2Color = []
-var column1 = document.getElementsByClassName('c1')
-var column2 = document.getElementsByClassName('c2')
-var column3 = document.getElementsByClassName('c3')
-var column4 = document.getElementsByClassName('c4')
-var column5 = document.getElementsByClassName('c5')
-var column6 = document.getElementsByClassName('c6')
-var column7 = document.getElementsByClassName('c7')
+var column1 = $('.c1')
+var column2 = $('.c2')
+var column3 = $('.c3')
+var column4 = $('.c4')
+var column5 = $('.c5')
+var column6 = $('.c6')
+var column7 = $('.c7')
 
+// Button starts as 'Begin Game' but upon clicking becomes 'Reset Game' and upon clicking reverts to 'Begin Game' etc.
+$('#reset').click(function() {
+    if($(this).text() == "Begin Game") {
+        $(this).text("Reset Game")
+    } else {
+        $(this).text("Begin Game")
+    }
+})
+
+$('.gameBoard').click( function() {
+    console.log('WHAT THE HELL AM I DOING?!?!?!?!')
+    
+})
