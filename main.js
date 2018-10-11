3d2b396057afc0fcd96557e70ee17a171131dc01
Matt Piestrak <mattpiestrak@Matts-MacBook-Pro.local>
2018-10-10 21:01:38 -0400

    Began to add logic to recognize columns and individual squares

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 // make object of each column with number as key, div as value
+
+// Adding class player1/player2 when clicked to change background color on lowest unfilled div to correct color
+var player1Color = []
+var player2Color = []
+var column1 = document.getElementsByClassName('c1')
+var column2 = document.getElementsByClassName('c2')
+var column3 = document.getElementsByClassName('c3')
+var column4 = document.getElementsByClassName('c4')
+var column5 = document.getElementsByClassName('c5')
+var column6 = document.getElementsByClassName('c6')
+var column7 = document.getElementsByClassName('c7')
+
